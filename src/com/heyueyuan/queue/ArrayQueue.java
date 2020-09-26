91d1a38d753682c1918d91f0cfa97404f659d965
heyueyuan901@gmail.com <Esteem940625@USA>
2020-09-26 18:46:25 -0400

    Initial commit

---
+++ src/com/heyueyuan/queue/ArrayQueue.java
@@ -0,0 +1,93 @@
+package com.heyueyuan.queue;
+
+import java.util.Scanner;
+
+public class ArrayQueue {
+    public static void main(String[] args) {
+        Queue queue = new Queue(3);
+        char key = ' ';
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while(loop) {
+            System.out.println("s, show");
+            System.out.println("e, exit");
+            System.out.println("a, add");
+            System.out.println("g, get");
+
+            key = scanner.next().charAt(0);
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("number");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g':
+                    try {
+                        int res = queue.getQueue();
+                        System.out.printf("%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("Exit");
+    }
+}
+
+class Queue {
+    private int maxSize;
+    private int front;
+    private int rear;
+    private int[] arr;
+
+    public Queue(int arrMaxSize) {
+        maxSize = arrMaxSize;
+        arr = new int[maxSize];
+        front = -1;
+        rear = -1;
+    }
+
+    public boolean isFull() {
+        return rear == maxSize - 1;
+    }
+
+    public boolean isEmpty() {
+        return rear == front;
+    }
+
+    public void addQueue(int n) {
+        if(isFull()){
+            System.out.println("Full");
+            return;
+        }
+        rear++;
+        arr[rear] = n;
+    }
+
+    public int getQueue(){
+        if(isEmpty()){
+            throw new RuntimeException("Empty");
+        }
+        front++;
+        return arr[front];
+    }
+
+    public void showQueue(){
+        if(isEmpty()){
+            System.out.println("Empty");
+        }
+        for (int i = 0; i< arr.length; i++) {
+            System.out.printf("arr[%d]=%d\n", i, arr[i]);
+        }
+    }
+}
